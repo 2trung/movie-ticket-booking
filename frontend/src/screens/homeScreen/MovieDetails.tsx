eccf0eac6f1d00763e06396554ec42a06f476f74
Duong <duong4432@gmail.com>
2024-03-24 23:30:22 +0700

    Movie Details init

---
+++ frontend/src/screens/homeScreen/MovieDetails.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import { View, Text } from 'react-native';
+
+const MovieDetails: React.FC = () => {
+    return (
+        <View>
+            <Text>Movie Details</Text>
+        </View>
+    );
+};
+export default MovieDetails;
